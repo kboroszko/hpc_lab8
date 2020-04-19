f6d6e3fdb869b36c5a69d5906f82eb1faad2d7af
kboroszko <kajetan.boroszko@gmail.com>
2020-04-19 19:28:41 +0200

    bugfix 3

--- generator-par.cpp
+++ generator-par.cpp
@@ -45,8 +45,8 @@ int main(int argc, char *argv[]) {
             return 0;
         }
     }
-
-    auto graph = createAndDistributeGraph(numVertices, numProcesses, myRank);
+    Graph * graph = nullptr;
+//    auto graph = createAndDistributeGraph(numVertices, numProcesses, myRank);
     if (graph == nullptr) {
         std::cerr << "Error creating the graph." << std::endl;
         MPI_Finalize();
